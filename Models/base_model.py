4d299c65fe39ee2edfeb6382940ddf6f4718dc99
Mokhaled2004 <mokhaled2004@gmail.com>
2024-06-14 04:00:55 -0700

    Solo Leveling

---
+++ Models/base_model.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Module base_model
+
+This Module contains a definition for BaseModel Class
+"""
+
+from MarketMate import Models
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    
+    """BaseModel Class"""
+    
+    def __init__(self, *args, **kwargs):
+        
+        """BaseModel Constructor"""
+        
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        
+        if kwargs is not None and len(kwargs) > 0 :
+            for k, v in kwargs.item():
+                if k in ['created_at', 'updated_at']:
+                    setattr(self, k, datetime.fromisoformat(v))
+                else:
+                    setattr(self, k, v)
+                    
+        else:
+            Models.storage.new(self)   
+            
+            
+    def __str__(self):
+            
+            """String Representation of BaseModel"""
+            
+            return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        
+        
+    def save(self):
+            
+            """Save Method"""
+            
+            self.updated_at = datetime.now()
+            Models.storage.save()
+            
+            
+    def to_dict(self):
+            
+            """to_dict Method"""
+            
+            rdict = self.__dict__.copy()
+            rdict['__class__'] = self.__class__.__name__
+            rdict['created_at'] = self.created_at.isoformat()
+            rdict['updated_at'] = self.updated_at.isoformat()
+            
+            return rdict
+        
+        
+        
+            
+                             
+                             
+    
